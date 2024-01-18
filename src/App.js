2f7d52f14b03faa10e70aaafa1b84e681b2e4744
Sheetal <sheetalpal3107@gmail.com>
2024-01-18 14:01:06 +0530

    File updated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Popup from './Popup';
+
+const App = () => {
+  return (
+    <div>
+      <Popup />
+    </div>
+  );
+};
+
+export default App;
+
+
